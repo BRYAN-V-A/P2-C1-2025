db52f66119c538d1319b68388c0143250a46b7fb
BRYAN-V-A <aranguizbryan112@gmail.com>
2025-10-02 12:38:34 -0300

    Update midiagrama.py

--- midiagrama.py
+++ midiagrama.py
@@ -3,12 +3,30 @@ class Cliente:
         self.__nombre: str = nombre
         self.__telefono: str = telefono
         self.__email: str = email
+    @property
+    def pedirNombreCliente(self):
+        return self.__nombre
+    @property
+    def pedirTelefono(self):
+        return self.__telefono
+    @property
+    def pedirEmail(self):
+        return self.__email
 
 class Repartidor:
     def __init__(self,RUT: str,nombre: str,telefono: str):
         self.__RUT: str = RUT
         self.__nombre: str = nombre
         self.__telefono: str = telefono
+    @property
+    def pedirRut(self):
+        return self.__RUT
+    @property
+    def pedirNombreRepartidor(self):
+        return self.__nombre
+    @property 
+    def pedirTelefonoRepartidor(self):
+        return self.__telefono
 
 class Pedido:
     def __init__(self,nPedido: int,fecha: str,total: int,datosCliente: Cliente):
@@ -16,6 +34,18 @@ class Pedido:
         self._fecha: str = fecha
         self._total: int = total
         self.datosCliente: Cliente = datosCliente
+    @property
+    def pedirNumeroPedido(self):
+        return self._nPedido
+    @property
+    def pedirFecha(self):
+        return self._fecha
+    @property
+    def pedirTotal(self):
+        return self._total
+    @property
+    def pedirDatosCliente(self):
+        return self.datosCliente
 
 class P_Local:
     def __init__(self,numeroMesa: int):
